2a08e5f65a52d0ca67576383d951ebf497891278
jesusrp98 <jesusrope98@outlook.com>
2020-04-02 17:44:37 +0200

    Added a bunch of tests

--- test/big_tip_test.dart
+++ test/big_tip_test.dart
@@ -23,22 +23,145 @@ class TestPage extends StatelessWidget {
 }
 
 void main() {
-  testWidgets('Correct pull of default icon settings', (tester) async {});
+  testWidgets('Correct pull of default icon settings', (tester) async {
+    final BigTip _bigTip = BigTip(
+      child: Icon(Icons.cake),
+    );
 
-  testWidgets('Using a custom icon with custom properties', (tester) async {});
+    await tester.pumpWidget(
+      TestPage(_bigTip),
+    );
 
-  testWidgets('Correct pull of default title settings', (tester) async {});
+    // Expecting default cake icon & theme
+    expect(find.byIcon(Icons.cake), findsOneWidget);
+    final iconTheme = tester.widget<IconTheme>(
+      find
+          .ancestor(
+            of: find.byIcon(Icons.cake),
+            matching: find.byType(IconTheme),
+          )
+          .first,
+    );
+    expect(iconTheme.data.size, 100);
+    expect(iconTheme.data.color, Color(0x8a000000));
+  });
+
+  testWidgets('Using a custom icon with custom properties', (tester) async {
+    final BigTip _bigTip = BigTip(
+      child: Icon(
+        Icons.cake,
+        size: 50,
+        color: Colors.red,
+      ),
+    );
+
+    await tester.pumpWidget(
+      TestPage(_bigTip),
+    );
+
+    // Expecting default cake icon & theme
+    expect(find.byIcon(Icons.cake), findsOneWidget);
+    final icon = tester.widget<Icon>(find.byIcon(Icons.cake));
+    expect(icon.size, 50);
+    expect(icon.color, Colors.red);
+  });
+
+  testWidgets('Correct pull of default title settings', (tester) async {
+    final BigTip _bigTip = BigTip(
+      title: Text('title'),
+    );
+
+    await tester.pumpWidget(
+      TestPage(_bigTip),
+    );
+
+    // Expecting default title text & theme
+    expect(find.text('title'), findsOneWidget);
+    final defaultTextStyle = tester.widget<DefaultTextStyle>(
+      find
+          .ancestor(
+            of: find.text('title'),
+            matching: find.byType(DefaultTextStyle),
+          )
+          .first,
+    );
+    expect(defaultTextStyle.style.color, Color(0xdd000000));
+    expect(defaultTextStyle.style.fontSize, 20);
+    expect(defaultTextStyle.style.fontWeight, FontWeight.w500);
+  });
 
   testWidgets(
     'Using a custom title text widget with custom properties',
-    (tester) async {},
+    (tester) async {
+      final BigTip _bigTip = BigTip(
+        title: Text(
+          'title',
+          style: TextStyle(
+            fontSize: 25,
+            color: Colors.red,
+          ),
+        ),
+      );
+
+      await tester.pumpWidget(
+        TestPage(_bigTip),
+      );
+
+      // Expecting custom title text & theme
+      expect(find.text('title'), findsOneWidget);
+      final text = tester.widget<Text>(find.text('title'));
+      expect(text.style.color, Colors.red);
+      expect(text.style.fontSize, 25);
+    },
   );
 
-  testWidgets('Correct pull of default subtitle settings', (tester) async {});
+  testWidgets('Correct pull of default subtitle settings', (tester) async {
+    final BigTip _bigTip = BigTip(
+      subtitle: Text('subtitle'),
+    );
+
+    await tester.pumpWidget(
+      TestPage(_bigTip),
+    );
+
+    // Expecting default subtitle text & theme
+    expect(find.text('subtitle'), findsOneWidget);
+    final defaultTextStyle = tester.widget<DefaultTextStyle>(
+      find
+          .ancestor(
+            of: find.text('subtitle'),
+            matching: find.byType(DefaultTextStyle),
+          )
+          .first,
+    );
+    expect(defaultTextStyle.style.color, Color(0xdd000000));
+    expect(defaultTextStyle.style.fontSize, 16);
+    expect(defaultTextStyle.style.fontWeight, FontWeight.w400);
+  });
 
   testWidgets(
     'Using a custom subtitle text widget with custom properties',
-    (tester) async {},
+    (tester) async {
+      final BigTip _bigTip = BigTip(
+        subtitle: Text(
+          'subtitle',
+          style: TextStyle(
+            fontSize: 15,
+            color: Colors.red,
+          ),
+        ),
+      );
+
+      await tester.pumpWidget(
+        TestPage(_bigTip),
+      );
+
+      // Expecting custom subtitle text & theme
+      expect(find.text('subtitle'), findsOneWidget);
+      final text = tester.widget<Text>(find.text('subtitle'));
+      expect(text.style.color, Colors.red);
+      expect(text.style.fontSize, 15);
+    },
   );
 
   testWidgets(
@@ -55,36 +178,242 @@ void main() {
 
   testWidgets('Testing action widget disable capabilities', (tester) async {});
 
-  testWidgets('Applies padding by default', (tester) async {});
+  testWidgets('Applies padding by default', (tester) async {
+    final BigTip _bigTip = BigTip(
+      child: Icon(Icons.cake),
+    );
+
+    await tester.pumpWidget(
+      TestPage(_bigTip),
+    );
+
+    // Expecting default padding (32)
+    final padding = tester.widget<Padding>(find.ancestor(
+      of: find.byIcon(Icons.cake),
+      matching: find.byType(Padding),
+    ));
+    expect(padding.padding, EdgeInsets.all(32));
+  });
 
   testWidgets(
     'Applies custom padding properties corerctly',
-    (tester) async {},
+    (tester) async {
+      final BigTip _bigTip = BigTip(
+        padding: EdgeInsets.all(16),
+        child: Icon(Icons.cake),
+      );
+
+      await tester.pumpWidget(
+        TestPage(_bigTip),
+      );
+
+      // Expecting default padding (32)
+      final padding = tester.widget<Padding>(find.ancestor(
+        of: find.byIcon(Icons.cake),
+        matching: find.byType(Padding),
+      ));
+      expect(padding.padding, EdgeInsets.all(16));
+    },
   );
 
-  testWidgets('Applies child space by default', (tester) async {});
+  testWidgets('Applies child space by default', (tester) async {
+    final BigTip _bigTip = BigTip(
+      child: Icon(Icons.cake),
+      title: Text('title'),
+    );
+
+    await tester.pumpWidget(
+      TestPage(_bigTip),
+    );
+
+    // Expecting default child space (22)
+    final childSpace = tester.widget<SizedBox>(
+      find
+          .descendant(
+            of: find.byType(BigTip),
+            matching: find.byType(SizedBox),
+          )
+          .at(1),
+    );
+    expect(childSpace.height, 22);
+  });
 
   testWidgets(
     'Applies custom child space properties corerctly',
-    (tester) async {},
+    (tester) async {
+      final BigTip _bigTip = BigTip(
+        space: 16,
+        child: Icon(Icons.cake),
+        title: Text('title'),
+      );
+
+      await tester.pumpWidget(
+        TestPage(_bigTip),
+      );
+
+      // Expecting default child space (22)
+      final childSpace = tester.widget<SizedBox>(
+        find
+            .descendant(
+              of: find.byType(BigTip),
+              matching: find.byType(SizedBox),
+            )
+            .at(1),
+      );
+      expect(childSpace.height, 16);
+    },
   );
 
-  testWidgets('Applies text space by default', (tester) async {});
+  testWidgets('Applies text space by default', (tester) async {
+    final BigTip _bigTip = BigTip(
+      title: Text('title'),
+      subtitle: Text('subtitle'),
+    );
+
+    await tester.pumpWidget(
+      TestPage(_bigTip),
+    );
+
+    // Expecting default child space (22)
+    final childSpace = tester.widget<SizedBox>(
+      find.descendant(
+        of: find.byType(BigTip),
+        matching: find.byType(SizedBox),
+      ),
+    );
+    expect(childSpace.height, 8);
+  });
 
   testWidgets(
     'Applies custom text space properties corerctly',
-    (tester) async {},
+    (tester) async {
+      final BigTip _bigTip = BigTip(
+        textSpace: 12,
+        title: Text('title'),
+        subtitle: Text('subtitle'),
+      );
+
+      await tester.pumpWidget(
+        TestPage(_bigTip),
+      );
+
+      // Expecting default child space (22)
+      final childSpace = tester.widget<SizedBox>(
+        find.descendant(
+          of: find.byType(BigTip),
+          matching: find.byType(SizedBox),
+        ),
+      );
+      expect(childSpace.height, 12);
+    },
   );
 
-  testWidgets('Renders child-only layout', (tester) async {});
+  testWidgets('Renders child-only layout', (tester) async {
+    final BigTip _bigTip = BigTip(
+      child: Icon(Icons.cake),
+    );
 
-  testWidgets('Renders titleonly layout', (tester) async {});
+    await tester.pumpWidget(
+      TestPage(_bigTip),
+    );
 
-  testWidgets('Renders subtitle-only layout', (tester) async {});
+    // Finds only the SizedBox containing the icon itself
+    expect(find.byType(SizedBox), findsOneWidget);
+  });
 
-  testWidgets('Renders child-and-title-only layout', (tester) async {});
+  testWidgets('Renders title-only layout', (tester) async {
+    final BigTip _bigTip = BigTip(
+      title: Text('title'),
+    );
 
-  testWidgets('Renders child-and-subtitle-only layout', (tester) async {});
+    await tester.pumpWidget(
+      TestPage(_bigTip),
+    );
 
-  testWidgets('Renders title-and-subtitle-only layout', (tester) async {});
+    // Finds no separator
+    expect(find.byType(SizedBox), findsNothing);
+  });
+
+  testWidgets('Renders subtitle-only layout', (tester) async {
+    final BigTip _bigTip = BigTip(
+      subtitle: Text('subtitle'),
+    );
+
+    await tester.pumpWidget(
+      TestPage(_bigTip),
+    );
+
+    // Finds no separator
+    expect(find.byType(SizedBox), findsNothing);
+  });
+
+  testWidgets('Renders child-and-title-only layout', (tester) async {
+    final BigTip _bigTip = BigTip(
+      child: Icon(Icons.cake),
+      title: Text('title'),
+    );
+
+    await tester.pumpWidget(
+      TestPage(_bigTip),
+    );
+
+    // Finds the Icon child and its separator
+    expect(find.byType(SizedBox), findsNWidgets(2));
+  });
+
+  testWidgets('Renders child-and-subtitle-only layout', (tester) async {
+    final BigTip _bigTip = BigTip(
+      child: Icon(Icons.cake),
+      subtitle: Text('subtitle'),
+    );
+
+    await tester.pumpWidget(
+      TestPage(_bigTip),
+    );
+
+    // Finds the Icon child and its separator
+    expect(find.byType(SizedBox), findsNWidgets(2));
+  });
+
+  testWidgets('Renders title-and-subtitle-only layout', (tester) async {
+    final BigTip _bigTip = BigTip(
+      title: Text('title'),
+      subtitle: Text('subtitle'),
+    );
+
+    await tester.pumpWidget(
+      TestPage(_bigTip),
+    );
+
+    // Finds only the text spacer widget
+    expect(find.byType(SizedBox), findsOneWidget);
+  });
+
+  testWidgets('Renders with action correctly', (tester) async {
+    final BigTip _bigTip = BigTip(
+      title: Text('title'),
+      action: Text('Action'),
+    );
+
+    await tester.pumpWidget(
+      TestPage(_bigTip),
+    );
+
+    // Finds only the text spacer widget
+    expect(find.byType(Flexible), findsNWidgets(2));
+  });
+
+  testWidgets('Renders without action correctly', (tester) async {
+    final BigTip _bigTip = BigTip(
+      title: Text('title'),
+      subtitle: Text('subtitle'),
+    );
+
+    await tester.pumpWidget(
+      TestPage(_bigTip),
+    );
+
+    // Finds only the text spacer widget
+    expect(find.byType(Flexible), findsNothing);
+  });
 }
